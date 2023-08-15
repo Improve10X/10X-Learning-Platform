fb7498754475b99d6bdcc91a4dda0f1012bd9339
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-15 21:27:15 +0530

    I created ContactUsActivity and xml

---
+++ app/src/main/java/com/improve10x/learningplatform/contactus/ContactUsActivity.java
@@ -0,0 +1,16 @@
+package com.improve10x.learningplatform.contactus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.improve10x.learningplatform.R;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
