6d9e96c5cba2c8cb6a91cd76b32ee47159b49e3f
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-08-16 16:58:02 +0530

    Added empty line

--- app/src/main/java/com/improve10x/learningplatform/contactus/SendMessageControllerImpl.java
+++ app/src/main/java/com/improve10x/learningplatform/contactus/SendMessageControllerImpl.java
@@ -1,6 +1,7 @@
 package com.improve10x.learningplatform.contactus;
 
 public class SendMessageControllerImpl implements SendMessageController {
+
     @Override
     public String sendMessage(String message) {
         String result = "";
