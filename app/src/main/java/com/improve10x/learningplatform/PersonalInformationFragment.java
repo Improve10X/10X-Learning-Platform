5a5bf41ebeb12eed981c7f6f8fc0c6d8c1476b5f
Anupama <anupama.lakshmi@Improve10x.com>
2023-08-16 14:58:26 +0530

    Modified PersonalInformationActivity as Fragment

---
+++ app/src/main/java/com/improve10x/learningplatform/PersonalInformationFragment.java
@@ -0,0 +1,17 @@
+package com.improve10x.learningplatform;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PersonalInformationFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_personal_information, container, false);
+    }
+}
