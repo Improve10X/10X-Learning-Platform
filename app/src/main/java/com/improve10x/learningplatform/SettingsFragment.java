d0ad538fb3c516910d8b55f16feef25246b85744
K Hareesh <hareesh.komma@improve10x.com>
2023-08-16 16:31:21 +0530

    I modified some changes

--- app/src/main/java/com/improve10x/learningplatform/SettingsFragment.java
+++ app/src/main/java/com/improve10x/learningplatform/SettingsFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
