4e3c8e49b8191c48c157c14294a18ea0c3a78b78
Anupama <anupama.lakshmi@Improve10x.com>
2023-08-16 14:41:26 +0530

    Created personal information activity and branch

---
+++ app/src/main/java/com/improve10x/learningplatform/PersonalInformationActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.learningplatform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_information);
+    }
+}
