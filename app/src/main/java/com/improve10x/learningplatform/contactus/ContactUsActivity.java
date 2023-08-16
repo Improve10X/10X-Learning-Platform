f0974a5dad75af386b87aeb62000cf4874f2d319
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-16 10:25:49 +0530

    I have designed Contact us xml

--- app/src/main/java/com/improve10x/learningplatform/contactus/ContactUsActivity.java
+++ app/src/main/java/com/improve10x/learningplatform/contactus/ContactUsActivity.java
@@ -1,13 +1,18 @@
 package com.improve10x.learningplatform.contactus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.improve10x.learningplatform.R;
 
 public class ContactUsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
