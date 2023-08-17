8dece007e7aa14c38ce534a0a716aa484652947d
Omprakash <omprakash.mopuri@improve10x.com>
2023-08-17 10:49:54 +0530

    Implemented loadFragment() method in WorkShopActivity.java

--- app/src/main/java/com/improve10x/learningplatform/workshops/WorkShopActivity.java
+++ app/src/main/java/com/improve10x/learningplatform/workshops/WorkShopActivity.java
@@ -1,9 +1,12 @@
 package com.improve10x.learningplatform.workshops;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
+import com.improve10x.learningplatform.R;
 import com.improve10x.learningplatform.databinding.ActivityWorkShopBinding;
 
 public class WorkShopActivity extends AppCompatActivity {
@@ -15,5 +18,13 @@ public class WorkShopActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityWorkShopBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        loadFragment(new DummyFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.root_layout, fragment)
+                .commit();
     }
 }
