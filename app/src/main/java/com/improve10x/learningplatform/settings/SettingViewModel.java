2110b44d6385d29f39338b3af3427eddc566e0ee
Harsha <shreeharsha.a@improve10x.com>
2023-08-17 17:45:39 +0530

    I added SettingViewModel

--- app/src/main/java/com/improve10x/learningplatform/settings/SettingViewModel.java
+++ app/src/main/java/com/improve10x/learningplatform/settings/SettingViewModel.java
@@ -8,5 +8,27 @@ public class SettingViewModel extends ViewModel {
     private MutableLiveData<Boolean> sendTestNotificationSwitch = new MutableLiveData<>();
     private MutableLiveData<Boolean> soundSwitch = new MutableLiveData<>();
 
+    public MutableLiveData<Boolean> getNotificationSwitch() {
+        return notificationSwitch;
+    }
 
+    public void setNotificationSwitch(MutableLiveData<Boolean> notificationSwitch) {
+        this.notificationSwitch = notificationSwitch;
+    }
+
+    public MutableLiveData<Boolean> getSendTestNotificationSwitch() {
+        return sendTestNotificationSwitch;
+    }
+
+    public void setSendTestNotificationSwitch(MutableLiveData<Boolean> sendTestNotificationSwitch) {
+        this.sendTestNotificationSwitch = sendTestNotificationSwitch;
+    }
+
+    public MutableLiveData<Boolean> getSoundSwitch() {
+        return soundSwitch;
+    }
+
+    public void setSoundSwitch(MutableLiveData<Boolean> soundSwitch) {
+        this.soundSwitch = soundSwitch;
+    }
 }
