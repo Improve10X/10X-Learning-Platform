ea0aee5051810485eb7566950428e38ae2742570
Harsha <shreeharsha.a@improve10x.com>
2023-08-17 17:57:04 +0530

    Fixed the comments

--- app/src/main/java/com/improve10x/learningplatform/settings/SettingViewModel.java
+++ app/src/main/java/com/improve10x/learningplatform/settings/SettingViewModel.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class SettingViewModel extends ViewModel {
+
     private MutableLiveData<Boolean> notificationSwitch = new MutableLiveData<>();
     private MutableLiveData<Boolean> sendTestNotificationSwitch = new MutableLiveData<>();
     private MutableLiveData<Boolean> soundSwitch = new MutableLiveData<>();
