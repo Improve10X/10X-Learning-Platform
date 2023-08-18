650430a548ad2fcfe3276938ba059b6945286cb4
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-18 17:52:17 +0530

    I have created Adapter class and used data binding in xml file

---
+++ app/src/main/java/com/improve10x/learningplatform/homecourses/CoursesAdapter.java
@@ -0,0 +1,42 @@
+package com.improve10x.learningplatform.homecourses;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.improve10x.learningplatform.databinding.CourseItemBinding;
+import com.improve10x.learningplatform.homecourses.model.Course;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class CoursesAdapter extends RecyclerView.Adapter<CourseViewHolder> {
+
+    private List<Course> courses;
+
+    public CoursesAdapter(List<Course> courses){
+        this.courses = courses;
+    }
+    @NonNull
+    @Override
+    public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        CourseItemBinding binding = CourseItemBinding.inflate(inflater, parent, false);
+        CourseViewHolder viewHolder = new CourseViewHolder(binding);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
+        Course course = courses.get(position);
+        holder.binding.setCourse(course);
+        Picasso.get().load(course.getImageUrl()).into(holder.binding.imageIv);
+    }
+
+    @Override
+    public int getItemCount() {
+        return courses.size();
+    }
+}
