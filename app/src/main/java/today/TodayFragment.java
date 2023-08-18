6b261bead7420f8af599d11d6d88f1dedbfa2395
Vinodkumar M <vinod.kumar@improve10x.com>
2023-08-18 17:07:39 +0530

    Removed Empty lines.

--- app/src/main/java/today/TodayFragment.java
+++ app/src/main/java/today/TodayFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 
 import com.improve10x.learningplatform.R;
 public class TodayFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
