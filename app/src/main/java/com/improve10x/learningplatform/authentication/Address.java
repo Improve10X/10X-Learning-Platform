6833a0ed2271b924c79f85319088b5b0cf4a10cf
Omprakash <omprakash.mopuri@improve10x.com>
2023-08-18 11:31:47 +0530

    Created getter and setter methods for all variables in Address.java and User.java

--- app/src/main/java/com/improve10x/learningplatform/authentication/Address.java
+++ app/src/main/java/com/improve10x/learningplatform/authentication/Address.java
@@ -5,4 +5,28 @@ public class Address {
     private String country;
     private String state;
     private String zipCode;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
--- app/src/main/java/com/improve10x/learningplatform/authentication/User.java
+++ app/src/main/java/com/improve10x/learningplatform/authentication/User.java
@@ -8,4 +8,52 @@ public class User {
     private Address address;
     private String email;
     private String mobileNumber;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 }
