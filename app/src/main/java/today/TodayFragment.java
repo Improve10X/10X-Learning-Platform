3afa6f8fb4bad22daed17ab904fc7d5317fe291f
Vinodkumar M <vinod.kumar@improve10x.com>
2023-08-18 15:29:47 +0530

    created TodayFragment.

---
+++ app/src/main/java/today/TodayFragment.java
@@ -0,0 +1,19 @@
+package today;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.improve10x.learningplatform.R;
+public class TodayFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_today, container, false);
+    }
+}
