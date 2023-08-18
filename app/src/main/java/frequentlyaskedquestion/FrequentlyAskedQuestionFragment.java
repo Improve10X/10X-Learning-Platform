ca1a4c9748d2430a11cca880cbcd531034bb82d5
Manohar <manohar.mangali@improve10x.com>
2023-08-18 11:42:13 +0530

    I create FrequentlyAskedQuestionFragment

---
+++ app/src/main/java/frequentlyaskedquestion/FrequentlyAskedQuestionFragment.java
@@ -0,0 +1,19 @@
+package frequentlyaskedquestion;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.improve10x.learningplatform.R;
+
+public class FrequentlyAskedQuestionFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_frequently_asked_question, container, false);
+    }
+}
