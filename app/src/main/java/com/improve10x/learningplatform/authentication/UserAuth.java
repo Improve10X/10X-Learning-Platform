52a8301a01567977a8784d86bcf25e5e78ad9840
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-18 15:45:05 +0530

    I added getCurrentUser() method in UserAuth.java class. implemented functionality.

--- app/src/main/java/com/improve10x/learningplatform/authentication/UserAuth.java
+++ app/src/main/java/com/improve10x/learningplatform/authentication/UserAuth.java
@@ -3,6 +3,7 @@ package com.improve10x.learningplatform.authentication;
 public class UserAuth {
 
     private static UserAuth instance;
+    private User user;
 
     private UserAuth() {
     }
@@ -10,7 +11,27 @@ public class UserAuth {
     public static UserAuth getInstance() {
         if (instance == null) {
             instance = new UserAuth();
+            instance.getDummyUser();
         }
         return instance;
+    }
+
+    public User getCurrentUser() {
+        return user;
+    }
+
+    private User getDummyUser() {
+        instance.user = new User();
+        instance.user.setUserId("1234");
+        instance.user.setName("MS Dhoni");
+        instance.user.setImageUrl("https://cdn.britannica.com/25/222725-050-170F622A/Indian-cricketer-Mahendra-Singh-Dhoni-2011.jpg");
+        Address address = new Address();
+        address.setCountry("India");
+        address.setState("Jharkhand");
+        address.setZipCode("99999");
+        instance.user.setAddress(address);
+        instance.user.setEmail("ms@gmail.com");
+        instance.user.setMobileNumber("9959256425");
+        return instance.user;
     }
 }
