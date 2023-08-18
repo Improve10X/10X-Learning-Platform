3ac46663271155d525094a52307fba8d4959dd8c
LENOVO <lakshmi.sannithi@improve10x.com>
2023-08-18 14:25:06 +0530

    I removed one line in xml.

---
+++ app/src/main/java/com/improve10x/learningplatform/demo/DemoActivity.java
@@ -0,0 +1,16 @@
+package com.improve10x.learningplatform.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.improve10x.learningplatform.R;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
