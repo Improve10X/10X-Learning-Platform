d8279f5d3c8bb14285d93efd12eee0d852815b61
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-08-18 14:53:18 +0530

    I Implemented UserAuth.java class singleton

--- app/src/main/java/com/improve10x/learningplatform/authentication/UserAuth.java
+++ app/src/main/java/com/improve10x/learningplatform/authentication/UserAuth.java
@@ -1,4 +1,16 @@
 package com.improve10x.learningplatform.authentication;
 
 public class UserAuth {
+
+    private static UserAuth instance;
+
+    private UserAuth() {
+    }
+
+    public static UserAuth getInstance() {
+        if (instance == null) {
+            instance = new UserAuth();
+        }
+        return instance;
+    }
 }
