8ea7f91a819caa4bc6b9673e72ea90bbbf9cbebf
LENOVO <lakshmi.sannithi@improve10x.com>
2023-08-21 16:20:07 +0530

    I wrote binding and methods for 3 Buttons in DemoActivity

--- app/src/main/java/com/improve10x/learningplatform/demo/DemoActivity.java
+++ app/src/main/java/com/improve10x/learningplatform/demo/DemoActivity.java
@@ -2,10 +2,15 @@ package com.improve10x.learningplatform.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.improve10x.learningplatform.R;
+import com.improve10x.learningplatform.contactus.ContactUsActivity;
 import com.improve10x.learningplatform.databinding.ActivityDemoBinding;
+import com.improve10x.learningplatform.profile.profilescreen.ProfileFragment;
+import com.improve10x.learningplatform.settings.SettingsFragment;
 
 public class DemoActivity extends AppCompatActivity {
     private ActivityDemoBinding binding;
@@ -15,9 +20,33 @@ public class DemoActivity extends AppCompatActivity {
         binding = ActivityDemoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         handleContactUsBtn();
+        handleProfileBtn();
+        handleSettingsBtn();
+    }
+
+    private void handleSettingsBtn() {
+        binding.settingsBtn.setOnClickListener(view -> {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, SettingsFragment.class);
+            startActivity(intent);
+        });
+    }
+
+    private void handleProfileBtn() {
+        binding.profileBtn.setOnClickListener(view -> {
+            Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, ProfileFragment.class);
+            startActivity(intent);
+        });
     }
 
     private void handleContactUsBtn() {
-
+        binding.contactusBtn.setOnClickListener(view -> {
+            Toast.makeText(this, "Contactus", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, ContactUsActivity.class);
+            startActivity(intent);
+        });
     }
+
+
 }
