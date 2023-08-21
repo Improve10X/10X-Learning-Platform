b24cd09cbe8baa36855e94e26a6c2d36c705edee
SivaKumar <sivakumar.g@improve10x.com>
2023-08-21 14:57:28 +0530

    I create new branch and added ne fragment containerview.

---
+++ app/src/main/java/com/improve10x/learningplatform/DummyResultActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.learningplatform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DummyResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_result);
+    }
+}
