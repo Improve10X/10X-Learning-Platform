8c7c954318e1cc1cdb63ae3cfe1f119645eab8a1
LENOVO <lakshmi.sannithi@improve10x.com>
2023-08-21 16:05:22 +0530

    I given id's for buttons in demo Xml.

--- app/src/main/java/com/improve10x/learningplatform/demo/DemoActivity.java
+++ app/src/main/java/com/improve10x/learningplatform/demo/DemoActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.improve10x.learningplatform.R;
+import com.improve10x.learningplatform.databinding.ActivityDemoBinding;
 
 public class DemoActivity extends AppCompatActivity {
-
+    private ActivityDemoBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_demo);
+        binding = ActivityDemoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        handleContactUsBtn();
+    }
+
+    private void handleContactUsBtn() {
+
     }
 }
